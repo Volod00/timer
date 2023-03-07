def298afb12533f337c4ffb5eb7d1bee6fd27381
Valdemar Yatsiv <evroprostir@gmail.com>
2023-03-07 12:28:35 +0200

    added function picker country

---
+++ countryPicker.js
@@ -0,0 +1,50 @@
+let countryData = [
+                {
+                    tags: ["confused", "sad"],
+                    isGif: true,
+                    image: "./country_images/india.jpg",
+                    alt: "India",
+                    text:'India - is a country for soul relax'
+
+                },
+                {
+                    tags: ["hungry", "moody"],
+                    isGif: true,
+                    image: "./country_images/korea.jpg",
+                    alt: "Korea",
+                    text: "You need something new in your life and you love food. Welcome to Korea",
+                },
+                {
+                    tags: ["relaxed", "sad"],
+                    isGif: true,
+                    image: "./country_images/thailand.jpg",
+                    alt: "Thailand",
+                    text: "Food, beaches, sun. Relax for soul, mind and body",
+                },
+                {
+                    tags: ["moody", "insomniac"],
+                    isGif: true,
+                    image: "./country_images/canada.jfif",
+                    alt: "Canada",
+                    text: "Countless places for relaxing nature activities",
+                },
+
+                {
+                    tags: ["exhausted", "insomniac"],
+                    isGif: true,
+                    image: "./country_images/new_zeland.jfif",
+                    alt: "New Zeland",
+                    text: "Here you'll never run out of opportunities for adventure ",
+                },
+                {
+                    tags: ["active"],
+                    isGif: false,
+                    image: "",
+                    alt: "Latvia",
+                    text: "Something you never forget",
+                },
+
+                
+            
+            ];
+export default countryData
---
+++ travelMenuData.js
@@ -0,0 +1,49 @@
+let travelData = [
+       {
+       src:"images/prague.jpg",
+       alt:"Europe",
+       title:"Chech Republic",
+       descripton:"Prague is famous for its cultural life. Wolfgang Amadeus Mozart lived there, and his Prague Symphony and Don Giovanni were first performed in the city.",
+       cost:250,
+       parentSelector:".trip .container",
+       },
+       {
+        src:"images/krakow.jpg",
+        alt:"Europe",
+        title:"Poland",
+        descripton:"Krakow is a must-see at any time of year but, as a Christmas destination, the city is up there with the best European cities. The short days and cold nights are the perfect excuse to warm up with mulled wine and indulge in some hearty Polish food, which you can find in abundance at the cosy Christmas market.",
+        cost: 200,
+        parentSelector:".trip .container",
+        },
+
+        {
+        src:"images/paris.jpg",
+        alt:"Europe",
+        title:"France",
+        descripton:"December is a wonderful time to visit Paris. The weather is cold and crisp, but the city is in full swing, with busy cafés, beautiful lights & decorations on the streets, and an interesting cultural program for locals and visitors alike.",
+        cost:650,
+        parentSelector:".trip .container",
+        },
+
+        {
+        src: "images/mexico.jpg",
+        alt: "Mexico City",
+        title:"Mexica",
+        descripton:"Mexico City is absolutely worth visiting because it is full of history (umpteen number of UNESCO sites in and around the city), is home to several wonderful museums and art galleries, is a haven for architecture lovers, and is the best place to dig into tacos and listen to some traditional Mariachi music",
+        cost:750,
+        parentSelector:".trip .container",
+        },
+
+        {
+        src: "images/New-York.jpg",
+        alt:"New York",
+        title:"USA",
+        descripton:"The best time to visit New York City is from April to June and September to early November when the weather is best and the crowds aren't as big. Summer and winter may also be good times to visit depending on your planned activities and budget.",
+        cost:1050,
+        parentSelector:".trip .container",
+        }
+]
+
+export default travelData 
+
+
